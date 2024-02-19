91b47630fb9cd041b3203348d9c6015ed50bb19c
Lerian Febriana, A.Md.Kom <kanglerian@gmail.com>
2024-02-19 16:47:30 +0700

    first commit

---
+++ migrations/20240219090834-create-member.js
@@ -0,0 +1,39 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Members', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      phone: {
+        type: Sequelize.STRING
+      },
+      notes: {
+        allowNull: true,
+        type: Sequelize.TEXT
+      },
+      status: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Members');
+  }
+};
---
+++ routes/members.js
@@ -0,0 +1,114 @@
+const express = require('express');
+const router = express.Router();
+const { Member } = require('../models');
+
+router.get('/', async (req, res) => {
+  let members = await Member.findAll();
+  return res.json(members);
+});
+
+router.get('/find/:phone', async (req, res) => {
+  let member = await Member.findOne({
+    where: {
+      phone: req.params.phone,
+    }
+  });
+
+  if(!member){
+    return res.json({
+      status: 404,
+      message: 'Anggota tidak ditemukan!'
+    });
+  }
+
+  return res.json({
+    status: 200,
+    message: 'Anggota ditemukan!',
+    data: member
+  });
+});
+
+router.get('/presence/:phone', async (req, res) => {
+  let member = await Member.findOne({
+    where: {
+      phone: req.params.phone,
+    }
+  });
+
+  if(!member){
+    return res.json({
+      status: 404,
+      message: 'Anggota tidak ditemukan!'
+    });
+  }
+
+  await Member.update({
+    status: true,
+  }, {
+    where: {
+      id: member.id
+    }
+  })
+
+  member = await Member.findOne({
+    where: {
+      id: member.id,
+    }
+  });
+
+  return res.json({
+    status: 200,
+    message: 'Anggota ditemukan!',
+    data: member
+  });
+});
+
+router.get('/cancel/:phone', async (req, res) => {
+  let member = await Member.findOne({
+    where: {
+      phone: req.params.phone,
+    }
+  });
+
+  if(!member){
+    return res.json({
+      status: 404,
+      message: 'Anggota tidak ditemukan!'
+    });
+  }
+
+  await Member.update({
+    status: false,
+  }, {
+    where: {
+      id: member.id
+    }
+  })
+
+  member = await Member.findOne({
+    where: {
+      id: member.id
+    }
+  });
+
+  return res.json({
+    status: 200,
+    message: 'Anggota ditemukan!',
+    data: member
+  });
+});
+
+router.post('/', async (req, res) => {
+  const { name, phone, notes } = req.body;
+  let data = {
+    name: name,
+    phone: phone,
+    notes: notes,
+  }
+  await Member.create(data);
+  return res.json({
+    message: 'Berhasil menambahkan anggota!'
+  });
+});
+
+module.exports = router;
